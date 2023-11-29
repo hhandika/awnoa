80eaf6b9c7dd2bb7233689415bf415526cd3ff7f
Heru Handika <herubiolog@gmail.com>
2023-11-29 20:55:24 +0700

    Add skeletons.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # awnoa
 
-A collaborative effort to develop a free, open source, and modular digital field guide for world fauna. We will start simple by developing a field guide for the Indonesian mammals. The work is a collaborative efforts among students, researchers, and international counterparts at the Indonesian Research and Innovation Agency (BRIN).
+A collaborative effort to develop a free, open source, and modular digital field guide for species identification. We will start simple by developing a field guide for the Indonesian mammals. The work is a collaborative efforts among students, researchers, and international counterparts at the Indonesian Research and Innovation Agency (BRIN).
---
+++ lib/home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool isListView = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('AWNOA Species ID'),
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isListView = !isListView;
+                });
+              },
+              icon: isListView
+                  ? const Icon(Icons.list_alt_rounded)
+                  : const Icon(Icons.grid_view_rounded),
+            ),
+          ],
+          automaticallyImplyLeading: false,
+        ),
+        body: const Center(
+          child: Text('Field guide for the mammals of Indonesia'),
+        ),
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: 0,
+          destinations: const [
+            NavigationDestination(
+              selectedIcon: Icon(Icons.search_rounded),
+              icon: Icon(Icons.search_outlined),
+              label: 'Explore',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.favorite_rounded),
+              icon: Icon(Icons.favorite_outline),
+              label: 'Favorites',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.menu_rounded),
+              icon: Icon(Icons.menu_outlined),
+              label: 'More',
+            ),
+          ],
+          onDestinationSelected: (index) {
+            setState(() {
+              switch (index) {
+                case 0:
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                  break;
+                case 1:
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                  break;
+                case 2:
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                  break;
+                case 3:
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                  break;
+              }
+            });
+          },
+        ));
+  }
+}
