13a7d56ab305dfb3ed7bf72abc13f2c308c37954
Heru Handika <herubiolog@gmail.com>
2023-12-01 06:36:40 +0700

    Add grid view.

--- lib/screens/explore.dart
+++ lib/screens/explore.dart
@@ -79,20 +79,24 @@ class SpeciesGridView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GridView.builder(
       itemCount: speciesList.length,
+      shrinkWrap: true,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
         childAspectRatio: 1.0,
+        mainAxisSpacing: 8.0,
+        crossAxisSpacing: 8.0,
       ),
       itemBuilder: (context, index) {
         return GridTile(
-          footer: GridTileBar(
-            backgroundColor: Colors.black45,
-            title: Text(speciesList[index].species,
-                style: Theme.of(context).textTheme.titleMedium),
-            subtitle: Text(speciesList[index].commonName),
-          ),
-          child: const Text('No image'),
-        );
+            footer: GridTileBar(
+              backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+              title: Text(speciesList[index].species,
+                  style: Theme.of(context).textTheme.titleMedium),
+              subtitle: Text(speciesList[index].commonName),
+            ),
+            child: const Center(
+              child: Text('Image'),
+            ));
       },
     );
   }
