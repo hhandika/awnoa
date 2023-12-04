6020326952ed716276002eb5e9d768d39636731c
Heru Handika <herubiolog@gmail.com>
2023-12-04 10:55:53 +0700

    Update dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:awnoa/screens/home.dart';
 import 'package:awnoa/styles/theme.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const AwnoaApp());
+  runApp(const ProviderScope(child: AwnoaApp()));
 }
 
 class AwnoaApp extends StatelessWidget {
